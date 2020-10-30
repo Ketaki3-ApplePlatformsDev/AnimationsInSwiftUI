921fd089c6d4d1106cfa2c0ad032a04c4159b8e0
Ketaki3-ApplePlatformsDev <ketaki3.apple@gmail.com>
2020-10-30 12:04:49 +0530

    ADD: Progress Bar, Documentation for Loading Text

--- LoadingIndicators/LoadingIndicators/LoadingText.swift
+++ LoadingIndicators/LoadingIndicators/LoadingText.swift
@@ -7,9 +7,16 @@
 
 import SwiftUI
 
+/**
+ * A view that creates a animated loading text.
+ */
 struct LoadingText: View {
+    /// A variable to tells how many dots to append
     @State var dotsToShow = ""
+    
+    /// A variable that creates timer publishing events on every 1 second
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         Text("Loading \(dotsToShow)")
             .font(.system(size: 20)).bold()
---
+++ LoadingIndicators/LoadingIndicators/ProgressBar.swift
@@ -0,0 +1,49 @@
+//
+//  ProgressBar.swift
+//  LoadingIndicators
+//
+//  Created by Ketaki Mahaveer Kurade on 30/10/20.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    /// A variable that creates timer publishing events on every 1 second
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    /// A variable that tells the  width of the increasing progress capsule
+    @State var width: CGFloat = 10
+    
+    /// A variable that tells by what amount the bar should progress
+    @State var progress: CGFloat = 10
+    
+    /// A variable that tells the total width of the bar
+    @State var capsuleWidth: CGFloat = 180
+    var body: some View {
+        ZStack {
+            Capsule()
+                .stroke(Color.gray)
+                .frame(width: capsuleWidth, height: 20)
+                .overlay(
+                    VStack (alignment: .leading) {
+                        Capsule()
+                            .fill(Color.green)
+                            .frame(width: self.width, height: 15, alignment: .leading)
+                            .padding([.leading, .trailing], 5)
+                            .animation(.linear(duration: 0.5))
+                    }.frame(width: capsuleWidth, height: 20, alignment: .leading)
+                ).onReceive(timer) { _ in
+                    self.width += self.progress
+                    if self.width >= self.capsuleWidth {
+                        self.width = 0
+                    }
+                }
+        }
+    }
+}
+
+struct ProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressBar()
+    }
+}
